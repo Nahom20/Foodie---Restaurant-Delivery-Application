87676f7e2509bd10fcd3d515fd1e6ca5a3508330
Lulit25 <lulitm5489@gmail.com>
2025-01-23 15:50:58 -0600

    add cors configuration

--- Microservice =-backend/resturant-listing/resturant-listing/src/main/java/com/foodie/resturant_listing/Controller/ResturantController.java
+++ Microservice =-backend/resturant-listing/resturant-listing/src/main/java/com/foodie/resturant_listing/Controller/ResturantController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/resturant")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public class ResturantController {
 
     @Autowired
--- Microservice =-backend/resturant-listing/resturant-listing/src/main/java/com/foodie/resturant_listing/cqrsconfig/CqrsConfig.java
+++ Microservice =-backend/resturant-listing/resturant-listing/src/main/java/com/foodie/resturant_listing/cqrsconfig/CqrsConfig.java
@@ -1,2 +1,25 @@
-package com.foodie.resturant_listing.cqrsconfig;public class CqrsConfig {
+package com.foodie.resturant_listing.cqrsconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CqrsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200") // Allow Angular frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
+
